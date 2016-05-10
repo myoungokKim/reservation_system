afe657d74e67fa9dfbb30861bc656d7809ade621
Myoungok Kim <cheese0414@naver.com>
2016-05-10 20:17:44 +0900

    #3 Second Event Programming

--- app/src/main/java/kr/hs/emirim/cheese0414/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/reservationsystem/MainActivity.java
@@ -29,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
         timePick.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE); //INVISIBLE은 화면에 이 뷰가 나타나지 않는다
 
+        radioDate.setOnClickListener(new View.OnClickListener() { //View를 import로 해도 ㅇㅋ
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.VISIBLE);
+                timePick.setVisibility(View.INVISIBLE);
+            }
+        });
+        radioTime.setOnClickListener(new View.OnClickListener() { //View를 import로 해도 ㅇㅋ
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.INVISIBLE);
+                timePick.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
