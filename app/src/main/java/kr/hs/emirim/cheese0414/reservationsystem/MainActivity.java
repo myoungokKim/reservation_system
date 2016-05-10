cd2439a7c16f95524e929f959d4dd722d94085b0
Myoungok Kim <cheese0414@naver.com>
2016-05-10 19:32:54 +0900

    #2 First Event Programming

--- app/src/main/java/kr/hs/emirim/cheese0414/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/reservationsystem/MainActivity.java
@@ -2,12 +2,32 @@ package kr.hs.emirim.cheese0414.reservationsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    Button butStart, butDone;
+    RadioButton radioDate, radioTime;
+    CalendarView calView;
+    TimePicker timePick;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chrono=(Chronometer)findViewById(R.id.chrono);
+        butStart=(Button)findViewById(R.id.but_start);
+        butDone=(Button)findViewById(R.id.but_done);
+        radioDate=(RadioButton)findViewById(R.id.radio_date);
+        radioTime=(RadioButton)findViewById(R.id.radio_time);
+        calView=(CalendarView)findViewById(R.id.calendar);
+        timePick=(TimePicker)findViewById(R.id.time_pick);
+        textResult=(TextView)findViewById(R.id.text_result);
+        timePick.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE); //INVISIBLE은 화면에 이 뷰가 나타나지 않는다
+
     }
 }
